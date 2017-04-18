899de668b6d03469f70e6d96baa8bb949da73e63
Alexandre DuBreuil <adu@lesfurets.com>
2017-04-18 23:55:55 +0200

    Add breizhcamp presentation and code

--- src/apache-spark/src/test/java/com/lesfurets/spark/examples/TarifsRunTest.java
+++ src/apache-spark/src/test/java/com/lesfurets/spark/examples/TarifsRunTest.java
@@ -1,7 +1,6 @@
 package com.lesfurets.spark.examples;
 
 import com.lesfurets.spark.junit5.extension.SparkTest;
-import com.lesfurets.spark.junit5.rule.SparkTest;
 import org.apache.spark.api.java.function.ForeachFunction;
 import org.apache.spark.broadcast.Broadcast;
 import org.apache.spark.sql.Dataset;
