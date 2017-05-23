659a53fa39b203bca827d4bbdf16e110f613eb2d
Alexandre DuBreuil <adu@lesfurets.com>
2017-05-23 13:31:34 +0200

    Correct minor elements jooq presentation

--- src/jOOQ/src/main/java/com/lesfurets/db/dao/BruJugFichesAutoDAO.java
+++ src/jOOQ/src/main/java/com/lesfurets/db/dao/BruJugFichesAutoDAO.java
@@ -1,9 +1,15 @@
 package com.lesfurets.db.dao;
 
 import com.lesfurets.model.FicheAuto;
+import org.jooq.Record7;
+import org.jooq.impl.DSL;
 
 import java.sql.Connection;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.lesfurets.db.tables.FichesAuto.FICHES_AUTO;
 
 public class BruJugFichesAutoDAO extends DAO {
 
@@ -11,8 +17,34 @@ public class BruJugFichesAutoDAO extends DAO {
         super(connection);
     }
 
-    public List<FicheAuto> selectRecentFiches(int statut) throws Exception {
-        return null;
+    public List<FicheAuto> selectRecentFiches(String offreUid, int statut) throws Exception {
+        return DSL
+                .using(connection)
+                .select(FICHES_AUTO.ID,
+                        FICHES_AUTO.OFFRE_UID,
+                        FICHES_AUTO.CREATION_DATE,
+                        FICHES_AUTO.CODE_POSTAL,
+                        FICHES_AUTO.EMAIL,
+                        FICHES_AUTO.CODE_SRA,
+                        FICHES_AUTO.STATUT)
+                .from(FICHES_AUTO)
+                .where(FICHES_AUTO.OFFRE_UID.eq(offreUid))
+                .and(FICHES_AUTO.STATUT.gt(statut))
+                .orderBy(FICHES_AUTO.STATUT.desc())
+                .stream()
+                .map(this::mapFicheAuto)
+                .collect(Collectors.toList());
+    }
+
+    private FicheAuto mapFicheAuto(Record7<Integer, String, LocalDate, String, String, String, Integer> record) {
+        return new FicheAuto(
+                FICHES_AUTO.ID.get(record),
+                FICHES_AUTO.OFFRE_UID.get(record),
+                FICHES_AUTO.CREATION_DATE.get(record),
+                FICHES_AUTO.CODE_POSTAL.get(record),
+                FICHES_AUTO.EMAIL.get(record),
+                FICHES_AUTO.CODE_SRA.get(record),
+                FICHES_AUTO.STATUT.get(record));
     }
 
 }
--- src/jOOQ/src/test/java/com/lesfurets/db/dao/FicheAutoDAOTest.java
+++ src/jOOQ/src/test/java/com/lesfurets/db/dao/FicheAutoDAOTest.java
@@ -15,21 +15,27 @@ public class FicheAutoDAOTest {
 
     private FicheAutoDAO ficheAutoDAO;
 
+    private BruJugFichesAutoDAO bruJugFichesAutoDAO;
+
     @Before
     public void setUp() throws Exception {
         Connection connection = ConnectionManager.getConnection();
         ConnectionManager.init(connection);
         legacyFicheAutoDAO = new LegacyFicheAutoDAO(connection);
         ficheAutoDAO = new FicheAutoDAO(connection);
+        bruJugFichesAutoDAO = new BruJugFichesAutoDAO(connection);
     }
 
     @Test
     public void should_have_no_regression() throws Exception {
         String uid = "0001495183264276b2c00000e6b55262";
         int statut = 100;
-        List<FicheAuto> reportFromLegacy = legacyFicheAutoDAO.selectRecentFiches(uid, statut);
-        List<FicheAuto> report = ficheAutoDAO.selectRecentFiches(uid, statut);
-        assertThat(report).isEqualTo(reportFromLegacy);
+        List<FicheAuto> fichesFromLegacy = legacyFicheAutoDAO.selectRecentFiches(uid, statut);
+        List<FicheAuto> fiches = ficheAutoDAO.selectRecentFiches(uid, statut);
+        assertThat(fiches).isEqualTo(fichesFromLegacy);
+
+        List<FicheAuto> fichesFromBruJug = bruJugFichesAutoDAO.selectRecentFiches(uid, statut);
+//        assertThat(fichesFromBruJug).isEqualTo(fichesFromLegacy);
     }
 
 }
