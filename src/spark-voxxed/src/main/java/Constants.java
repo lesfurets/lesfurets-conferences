919838bfc7cb8574f10e9b1aa002d62a3fd3a029
aar <andrei.arion@lesfurets.com>
2017-06-19 17:45:48 +0200

    Voxxed slides update

---
+++ src/spark-voxxed/src/main/java/Constants.java
@@ -0,0 +1,16 @@
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+public class Constants {
+
+    public static final String PATH_TARIFS_CSV = "src/main/resources/tarifs.csv";
+
+    public static final String PATH_WORDS_CSV = "src/main/resources/words.csv";
+
+    static {
+        Logger.getRootLogger().setLevel(Level.WARN);
+        Logger.getLogger("org").setLevel(Level.WARN);
+        Logger.getLogger("akka").setLevel(Level.WARN);
+    }
+
+}
---
+++ src/spark-voxxed/src/test/java/Step1Test.scala
@@ -0,0 +1,8 @@
+import org.scalatest.FunSuite
+
+/**
+  * Created by aar on 6/17/17.
+  */
+class Step1Test extends FunSuite {
+
+}
